b0c7e090560232cadedb6f180d5414f0edcc6b2e
Ender-Fake <segushkadno5576@gmail.com>
2026-07-08 03:14:50 +0300

    cleanup

--- src/client/java/com/enderium/physicsswapping/client/SwapUtils.java
+++ src/client/java/com/enderium/physicsswapping/client/SwapUtils.java
@@ -13,7 +13,6 @@ public class SwapUtils {
     private static final Minecraft MC = Minecraft.getInstance();
 
     public static void processSlot(int containerId, int stateId, short slotNum, byte buttonNum, ContainerInput containerInput, Int2ObjectMap<HashedStack> changedSlots, HashedStack carriedItem) {
-        //if (true)return;
         if (containerInput == ContainerInput.THROW) return;
         if (containerInput == ContainerInput.CLONE) return;
 
@@ -24,24 +23,12 @@ public class SwapUtils {
             if (stack == HashedStack.EMPTY) GuiRenderProcessor.removeSlot(i);
             else GuiRenderProcessor.addSlot(i, SwapSource.PACKET);
         });
-        //String string = changedSlots.int2ObjectEntrySet().stream().map(e -> e.getIntKey() + ": " + item(e.getValue())).toList().toString();
-        //System.out.printf("%s %s%n", containerInput, string);
-//         System.out.printf("%d %s %s %s %s %s %s%n", containerId,stateId, containerInput, slotNum, buttonNum, carriedItem, changedSlots);
-
-
     }
 
     public static void onChangeSlot(int containerId, int slot, ItemStack stack) {
         if (stack.isEmpty()) return;
         GuiRenderProcessor.addSlot(slot, SwapSource.SET_CLOT);
-        //System.out.printf("%s %s%n", containerId, slot);
-
     }
 
-
-    private static String item(HashedStack stack) {
-        if (stack instanceof HashedStack.ActualItem actualItem) return actualItem.item().getRegisteredName();
-        return stack.toString();
-    }
 
 }
