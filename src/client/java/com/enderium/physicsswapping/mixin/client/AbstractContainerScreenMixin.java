ff7304ce9a86ce6327c3011a43be3a0592da424a
Ender-Fake <segushkadno5576@gmail.com>
2026-07-22 15:54:05 +0300

    replace @Local Item to slot getItem

--- src/client/java/com/enderium/physicsswapping/mixin/client/AbstractContainerScreenMixin.java
+++ src/client/java/com/enderium/physicsswapping/mixin/client/AbstractContainerScreenMixin.java
@@ -2,11 +2,9 @@ package com.enderium.physicsswapping.mixin.client;
 
 import com.enderium.physicsswapping.client.render.GuiRenderProcessor;
 import com.enderium.physicsswapping.client.render.RenderContext;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,8 +14,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class AbstractContainerScreenMixin {
 
     @Inject(method = "extractSlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/Slot;isFake()Z"))
-    public void extractSlot(GuiGraphicsExtractor graphics, Slot slot, int mouseX, int mouseY, CallbackInfo ci, @Local(name = "itemStack") ItemStack itemStack) {
-        GuiRenderProcessor.extractItemInSlot(slot.index, itemStack);
+    public void extractSlot(GuiGraphicsExtractor graphics, Slot slot, int mouseX, int mouseY, CallbackInfo ci) {
+        GuiRenderProcessor.extractItemInSlot(slot.index, slot.getItem());
 
     }
 
